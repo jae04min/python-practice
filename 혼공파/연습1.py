8661509b2630492f51942f3d6c4458781557fbe9
jae04min <132644868+jae04min@users.noreply.github.com>
2024-03-21 01:12:57 +0900

    Add files via upload

---
+++ 혼공파/연습1.py
@@ -0,0 +1,9 @@
+numbers = [1,2,3,4,5,6]
+r_num = reversed(numbers)
+
+print(next(r_num))          #next()함수를 사용해서 내부의 요소를 하나하나 꺼낼 수 있다
+print(next(r_num))          #next()함수를 사용하지 않으면 reversed()함수의 리턴값이 나온다.
+print(next(r_num))
+print(next(r_num))
+print(next(r_num))
+print(next(r_num))
